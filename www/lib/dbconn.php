0850bf1632bcfed34c71ff591488df2efd911684
helle99 <au1gust8@gmail.com>
2020-11-09 15:50:32 +0900

    delete SNSfile

--- www/lib/dbconn.php
+++ www/lib/dbconn.php
@@ -1,6 +1,6 @@
 <?
-    $connect=mysql_connect( "localhost", "helle99", "wnddldua818") or  
+    $connect=mysql_connect( "localhost", "", "") or  
         die( "SQL server에 연결할 수 없습니다."); 
 
-    mysql_select_db("helle99",$connect);
+    mysql_select_db("",$connect);
 ?>
